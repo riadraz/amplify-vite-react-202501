ebf20206786d8aa63b7f219eaf33983fed0d76b3
riadraz <riadraz@gmail.com>
2025-01-30 18:02:54 +0600

    fourth Commit 20250112

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,57 +1,47 @@
-import { defineAuth } from "@aws-amplify/backend";
+import { defineAuth, secret } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
-        // Add social providers
-    social: {
+    externalProviders: {
       google: {
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET')
+      },
+      signInWithApple: {
+        clientId: secret('SIWA_CLIENT_ID'),
+        keyId: secret('SIWA_KEY_ID'),
+        privateKey: secret('SIWA_PRIVATE_KEY'),
+        teamId: secret('SIWA_TEAM_ID')
+      },
+      loginWithAmazon: {
+        clientId: secret('LOGINWITHAMAZON_CLIENT_ID'),
+        clientSecret: secret('LOGINWITHAMAZON_CLIENT_SECRET')
       },
       facebook: {
-        clientId: process.env.FACEBOOK_CLIENT_ID,
-        clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+        clientId: secret('FACEBOOK_CLIENT_ID'),
+        clientSecret: secret('FACEBOOK_CLIENT_SECRET')
       },
-   /*
-      amazon: {
-        clientId: process.env.AMAZON_CLIENT_ID,
-        clientSecret: process.env.AMAZON_CLIENT_SECRET,
-      },
-      apple: {
-        clientId: process.env.APPLE_CLIENT_ID,
-        teamId: process.env.APPLE_TEAM_ID,
-        keyId: process.env.APPLE_KEY_ID,
-        privateKey: process.env.APPLE_PRIVATE_KEY,
-      }
-   */
-    },
-    // Configure callback and logout URLs
-    oauth: {
-      callbackUrls: ['http://localhost:5173/'], // Add your callback URLs
-      logoutUrls: ['http://localhost:5173/'], // Add your logout URLs
-      scopes: ['email', 'profile', 'openid']
+      callbackUrls: [
+        'http://localhost:5173/profile',
+        'https://mywebsite.com/profile'
+      ],
+      logoutUrls: ['http://localhost:5173/', 'https://mywebsite.com'],
     }
   }
 });
 
-
-
 /*
 import { defineAuth } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+
+ // Define and configure your auth resource
+ // @see https://docs.amplify.aws/gen2/build-a-backend/auth
  
+
 export const auth = defineAuth({
   loginWith: {
     email: true,
   },
 });
-
 */
