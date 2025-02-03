3d91702dce80e8cbcdfc4431109388269355def3
riadraz <riadraz@gmail.com>
2025-02-03 14:15:24 +0600

    Twelvth Commit 20250204

--- src/pages/Inventory.tsx
+++ src/pages/Inventory.tsx
@@ -1,32 +1,18 @@
 import MultiSelect from '../components/ui/MultiSelect';
 import SearchableSelect from '../components/ui/SearchableSelect';
 
-
-  const options = [
-    { value: '1', label: 'Computer' },
-    { value: '2', label: 'Game Consol' },
-    { value: '3', label: 'Fridge' },
-    { value: '4', label: 'Laptop' },
-  ];
-
-  const handleChange = (value: string) => {
-    console.log('Selected value:', value);
-  };
-
+  
   
 export default function Inventory() {
   return (
     <div>
       <h1>Product Selection</h1>
       <MultiSelect />
-      <SearchableSelect
-      options={options}
-      onChange={handleChange}
-      placeholder="Select an option..."
-      defaultValue="1"
-      className="custom-select"
-      disabled={false}
-    />
+      <div style={{ padding: '20px' }}>
+        <h1>Product Selector</h1>
+        <SearchableSelect />
+      </div>
+  
     </div>
   );
 }
